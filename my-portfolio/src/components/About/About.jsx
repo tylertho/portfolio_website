0e1fef47fe092396f8415885e433b7591e2f94eb
Tyler Roth <tylerroth@Tylers-MBP-2.lan>
2024-08-30 21:34:05 -0400

    Website finished

---
+++ my-portfolio/src/components/About/About.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { getImageUrl } from '../../utils';
+import styles from "./About.module.css";
+
+export const About = () => {
+    return (
+        <section className={styles.container} id="about">
+            <h2 className={styles.title}>
+                About
+            </h2>
+            <div className={styles.content}>
+                <img src={getImageUrl("about/headshot.png")} alt="me" className={styles.aboutImage}/>
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
+                        {/* <img src={getImageUrl("about.cursoIcon.png")} alt="also me"
+                        className={styles.aboutImage}/> */}
+                        <div className={styles.aboutItemText}>
+                            <h3>
+                                Software Developer
+                            </h3>
+                            <p>
+                                I'm a software developer.
+                            </p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        {/* <img src={getImageUrl("about.serverIcon.png")} alt="also me"/> */}
+                        <div className={styles.aboutItemText}>
+                            <h3>
+                                Computer Science Student
+                            </h3>
+                            <p>
+                                I'm a computer science student at the University of North Carolina at Chapel Hill.
+                            </p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        {/* <img src={getImageUrl("about.uhhh.png")} alt="also me"/> */}
+                        <div className={styles.aboutItemText}>
+                            <h3>
+                                Hobbies
+                            </h3>
+                            <p>
+                                Aside from programming, I'm an avid sports fan. I favorite teams are the Packers, Manchester United, Suns, and UNC.
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            
+        </section>
+    )
+}
---
+++ my-portfolio/src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import styles from "./ProjectCard.module.css";
+import { getImageUrl } from '../../utils';
+
+
+export const ProjectCard = ({ project: {title, description, skills, demo, imageSrc, source} }) => {
+    return (
+        <div className={styles.container}>
+            <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image}/>
+            <h3 className={styles.title}>
+                {title}
+            </h3>
+            <p className={styles.description}>
+                {description}
+            </p>
+            <ul className={styles.skills}>{
+                skills.map((skill, id) => {
+                    return (
+                    <li key={id} className={styles.skill}>
+                        {skill}
+                    </li>
+                    );
+                })
+            }</ul>
+            <div className={styles.links}>
+                {demo && (
+                    <a href={demo} className={styles.link}>Demo</a>
+                )}
+                {source && (
+                    <a href={source} className={styles.link}>Source</a>
+                )}
+            </div>
+        </div>
+    )
+}
